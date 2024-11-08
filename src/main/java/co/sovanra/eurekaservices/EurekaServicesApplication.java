597e6bcba90e267216077a7cc12ab24fcb2678d6
ruos-sovanra <vannraruos@gmail.com>
2024-11-08 15:10:41 +0700

    Initial commit

---
+++ src/main/java/co/sovanra/eurekaservices/EurekaServicesApplication.java
@@ -0,0 +1,13 @@
+package co.sovanra.eurekaservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServicesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServicesApplication.class, args);
+    }
+}
